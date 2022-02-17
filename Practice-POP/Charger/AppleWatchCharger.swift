513f56d6bb0ff198bccdf543362c9013f26d07f3
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-17 22:24:05 +0900

    feat: AppleWatchCharger 구현

---
+++ Practice-POP/Charger/AppleWatchCharger.swift
@@ -0,0 +1,19 @@
+//
+//  AppleWatchCharger.swift
+//  Practice-POP
+//
+//  Created by 이지원 on 2022/02/17.
+//
+
+import Foundation
+
+struct AppleWatchCharger: Chargeable {
+    var chargemAhPerHour: Double = 10.0
+    
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < chargemAhPerHour {
+            return chargeablemAhPerHour
+        }
+        return chargemAhPerHour
+    }
+}
