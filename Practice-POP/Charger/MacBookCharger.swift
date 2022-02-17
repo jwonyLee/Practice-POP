92ecb6fce8675cec91ce076c4e1b5b53a0da8eb3
JiWon Lee <jiwon.swdev@gmail.com>
2022-02-17 22:26:40 +0900

    feat: MacBookCharger 구현

---
+++ Practice-POP/Charger/MacBookCharger.swift
@@ -0,0 +1,19 @@
+//
+//  MacBookCharger.swift
+//  Practice-POP
+//
+//  Created by 이지원 on 2022/02/17.
+//
+
+import Foundation
+
+struct MacBookCharger: Chargeable {
+    var chargemAhPerHour: Double = 50.0
+    
+    func convert(chargeablemAhPerHour: Double) -> Double {
+        if chargeablemAhPerHour < chargemAhPerHour {
+            return chargeablemAhPerHour
+        }
+        return chargemAhPerHour
+    }
+}
